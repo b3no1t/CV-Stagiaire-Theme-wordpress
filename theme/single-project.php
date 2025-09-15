b9b37193083744055b711fc06e90c4ffe43cda38
Benoit Lepage <workaholiks@gmail.com>
2025-09-15 13:36:21 +0200

    cpt

--- theme/single-project.php
+++ theme/single-project.php
@@ -9,8 +9,6 @@ $context         = Timber::context();
 $timber_post     = Timber::get_post();
 $context['post'] = $timber_post;
 
-
-
 if ( post_password_required( $timber_post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
 } else {
